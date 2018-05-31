df3eeef52cad56759fa85fcf587bdd045356eb9f
alejandroclose@gmail.com <alejandroclose@gmail.com>
2018-05-31 21:33:20 +0200

    done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 console.log('Hello!!');
+
+window.onload = function(){
+
+// Añadir línea en el ul
+ $('.btn').click((function(){
+   var line = (`<li><span>${prompt("Add new task")}</span><div class="delete">delete</div></li>`);
+   $('#todos').append(line);
+ }));
+
+
+// al hacer clic en delete que está dentro del li y de la clase todos eliminarlo.
+ $('#todos').on("click", "li .delete", function(){
+  $(this).parent().remove();
+});
+
+};
